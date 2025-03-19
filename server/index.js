44f234defaf37241c62041b612d5ae039f01be21
DarshanK2403 <darshandeesa2403@gmail.com>
2025-03-19 09:09:16 +0530

    Fix error

--- server/index.js
+++ server/index.js
@@ -4,7 +4,6 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 const mongoose = require('mongoose');
 const cors = require('cors');
-const morgan = require("morgan");
 
 app.use(cors({
   origin: "http://localhost:5173", // Allow requests from any origin (you can restrict it later)
@@ -13,7 +12,6 @@ app.use(cors({
 }));
 
 app.use(express.json({ limit: '50mb' }));
-app.use(morgan("dev")); // Log requests
 
 mongoose.connect(process.env.MONGO_URI, {
 });
