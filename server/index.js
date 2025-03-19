6e1aa193aca98f3f5eb02c7160ffe19ce6573b19
DarshanK2403 <darshandeesa2403@gmail.com>
2025-03-19 09:25:48 +0530

    origin update

--- server/index.js
+++ server/index.js
@@ -4,14 +4,16 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 const mongoose = require('mongoose');
 const cors = require('cors');
+const morgan = require("morgan");
 
 app.use(cors({
-  origin: "http://localhost:5173", // Allow requests from any origin (you can restrict it later)
+  origin: "https://financetrack-blond.vercel.app", // Allow requests from any origin (you can restrict it later)
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
 }));
 
 app.use(express.json({ limit: '50mb' }));
+app.use(morgan("dev")); // Log requests
 
 mongoose.connect(process.env.MONGO_URI, {
 });
